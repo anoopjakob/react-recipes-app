746ee975afbf891d979fdde7e9f6c5a17e367058
Anoop Jacob <anupjakob@gmail.com>
2020-10-09 16:59:10 +0530

    Updates

---
+++ precache-manifest.6318c92d60fe42eaabde3419e9289666.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c614e484b4f54e98faee7a78811c0f32",
+    "url": "/react-recipes-app/index.html"
+  },
+  {
+    "revision": "27e5a88a679b12927d0b",
+    "url": "/react-recipes-app/static/css/main.51543e74.chunk.css"
+  },
+  {
+    "revision": "05fb40bf0dbbbef7377e",
+    "url": "/react-recipes-app/static/js/2.35f3cf29.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-recipes-app/static/js/2.35f3cf29.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "27e5a88a679b12927d0b",
+    "url": "/react-recipes-app/static/js/main.70f78f17.chunk.js"
+  },
+  {
+    "revision": "ec63b6b21398c60b0aac",
+    "url": "/react-recipes-app/static/js/runtime-main.435cd5c9.js"
+  }
+]);
